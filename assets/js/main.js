7200a5102485755a2da74a8d1d1fa3b43e51208a
felixxsin <felix020@binus.ac.id>
2023-09-24 14:03:21 +0700

    Navbar Product

--- assets/js/main.js
+++ assets/js/main.js
@@ -356,3 +356,42 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+ // Get the "Product" list item and its dropdown menu
+ const productDropdown = document.getElementById("product-dropdown");
+ const productDropdownMenu = productDropdown.querySelector("ul");
+
+ // Add event listeners
+ productDropdown.addEventListener("mouseenter", () => {
+   // Show the dropdown menu on mouse enter
+   productDropdownMenu.style.display = "block";
+ });
+
+ productDropdown.addEventListener("mouseleave", () => {
+   // Hide the dropdown menu on mouse leave
+   productDropdownMenu.style.display = "none";
+ });
+
+ // Add a click event listener to navigate to the product page for the main "Product" link
+const mainProductLink = productDropdown.querySelector("a");
+mainProductLink.addEventListener("click", (e) => {
+  // Prevent the default link behavior for the main "Product" link
+  e.preventDefault();
+
+  // Redirect to the product page
+  window.location.href = "product.html";
+});
+
+// Update the links for nested dropdown items (VRF, Gree, Daikin)
+const nestedLinks = productDropdownMenu.querySelectorAll("a");
+
+nestedLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    // Prevent the default link behavior
+    e.preventDefault();
+
+    // Redirect to the linked page
+    window.location.href = link.getAttribute("href");
+  });
+});
+
